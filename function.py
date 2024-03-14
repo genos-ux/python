fa80dbdd6c7d4e240d7e425663c9feb9b3557522
genos-ux <gabriel.kyeremateng.gk@gmail.com>
2024-03-14 00:49:12 +0000

    edited

--- function.py
+++ function.py
@@ -1,8 +1,6 @@
 def calculate_discount(price,discount_percent):
     if(discount_percent >= 0.2):
-        return price * discount_percent
+        print(price * discount_percent)
 
-    return price
-
-print(calculate_discount(1000,0.1))
-
+    else:
+        print(price)
